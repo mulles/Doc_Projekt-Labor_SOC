f84ce6c3b8067e949aa8b948f48e4fe5377510e5
mulles <github.com@emu.lu>
2021-10-22 23:13:56 +0200

    Graph generated and embed inside the latex document based on data from the other repo

---
+++ graphs.py
@@ -0,0 +1,16 @@
+import os 
+import pandas as pd
+currentWorkingDir =  os.getcwd()
+outputDataDir = currentWorkingDir + '/dep/kalman-soc/data/'
+dfProcessedSensorDataEnhanced = pd.read_csv(outputDataDir + 'EVLKpN_20210817-20210830_enhanced_processed_sensor_data.zip')  
+
+#Matplotlib
+#dfProcessedSensorDataEnhanced.plot()
+#plt.show()
+
+#Plotly
+pd.options.plotting.backend = "plotly"
+fig = dfProcessedSensorDataEnhanced.plot()
+fig.update_layout(title_text='<b> Overview  </b>', title_x=0.5)
+fig.show()
+fig.write_image("overview.pdf")
