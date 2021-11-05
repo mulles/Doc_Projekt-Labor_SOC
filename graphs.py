6f5d6c4ca7cd5cad034784708478e867515ba7b0
mulles <github.com@emu.lu>
2021-11-05 10:37:52 +0100

    Chapter 3 coming together

--- graphs.py
+++ graphs.py
@@ -1,17 +1,29 @@
 import os 
 import pandas as pd
+from matplotlib.pylab import *
 currentWorkingDir =  os.getcwd()
 outputDataDir = currentWorkingDir + '/dep/kalman-soc/data/'
 dfProcessedSensorDataEnhanced = pd.read_csv(outputDataDir + 'EVLKpN_20210817-20210830_enhanced_processed_sensor_data.zip')  
 
-#Matplotlib
-#dfProcessedSensorDataEnhanced.plot()
-#plt.show()
-
-#Plotly
-pd.options.plotting.backend = "plotly"
-fig = dfProcessedSensorDataEnhanced.plot()
-fig.update_layout(title_text='<b> Overview  </b>', title_x=0.5)
-fig.show()
-fig.write_image("overview.pdf")
--m 
+x = dfProcessedSensorDataEnhanced.index *5/60
+yEKF = dfProcessedSensorDataEnhanced.kalman_soc * 10
+ySoCLS = dfProcessedSensorDataEnhanced.SoC_LibreSolar * 10
+# rc('text', usetex=True)
+# rc('font', **{'family':'serif', 'serif':['Times']})
+# rc('font', size=10.0)			
+# rc('legend', fontsize=10.0)
+#x = linspace(0, 3*pi)
+figure(figsize=(6,2))
+plot(x,yEKF,label='kalman_soc') 
+plot(x,ySoCLS,label='SoC_LibreSolar') #,linestyle='dashed'
+xlabel(r'time (h)')
+ylabel(r'SoC (%)')
+grid()
+#xticks(arange(0, 4*pi, pi), ('$0$',
+#'$\pi$', '$2\pi$', '$3\pi$'))
+#axis([0, 3*pi, -1, 1])
+legend(loc='lower right')
+savefig('librevskalman.pdf', bbox_inches='tight')
+dfProcessedSensorDataEnhanced.plot()
+plt.show()
+savefig('matplotoverview.pdf')
